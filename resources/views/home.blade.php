2f2c9ec1c5a3e2b0b2d635f0538af64cab88204a
Rangmon <ken371178@gmail.com>
2025-06-04 10:37:52 +0700

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite(['resources/js/app.js'])
+  <title>Document</title>
+
+</head>
+<body>
+  <!-- Nav -->
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">EssenSea</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pruduct</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Kategori
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Male</a></li>
+            <li><a class="dropdown-item" href="#">Female</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Unisex</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Login</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+  <!-- Card -->
+   <div class="container my-4">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4">
+      <div class="col">
+        <div class="card">
+          <img src="https://th.bing.com/th/id/OIP.fTPCp69ZrMbRLWyHTp3iywHaHa?r=0&rs=1&pid=ImgDetMain" class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <h5>UnSeen</h5>
+            <p class="card-text">Rp.</p>
+            <a href="#" class="btn btn-primary">Beli Sekarang</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card border-primary">
+          <img src="https://th.bing.com/th/id/OIP.fTPCp69ZrMbRLWyHTp3iywHaHa?r=0&rs=1&pid=ImgDetMain" class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <h5>UnSeen</h5>
+            <p class="card-text">Rp.</p>
+            <a href="#" class="btn btn-primary">Beli Sekarang</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="https://th.bing.com/th/id/OIP.fTPCp69ZrMbRLWyHTp3iywHaHa?r=0&rs=1&pid=ImgDetMain" class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <h5>UnSeen</h5>
+            <p class="card-text">Rp.</p>
+            <a href="#" class="btn btn-primary">Beli Sekarang</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="https://th.bing.com/th/id/OIP.fTPCp69ZrMbRLWyHTp3iywHaHa?r=0&rs=1&pid=ImgDetMain" class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <h5>UnSeen</h5>
+            <p class="card-text">Rp.</p>
+            <a href="#" class="btn btn-primary">Beli Sekarang</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<footer class="bd-footer py-4 py-md-5 mt-5 bg-body-tertiary">
+<div class="bd-footer py-4 py-md-5 mt-5 bg-body-tertiary">
+  <div class="row">
+  <div class="col-lg-3 mb-3">
+     <a class="d-inline-flex align-items-center mb-2 text-body-emphasis text-decoration-none">
+      <span class="fs-5">EssenSea</span>
+    </a>
+    <ul class="list-unstyled small">
+      <li class="mb-2">
+        Parfum Bau kadal
+      </li>
+    </ul>
+    </div>  
+  </div>
+</div>
+</footer>
+
+
+</body>
+</html>
